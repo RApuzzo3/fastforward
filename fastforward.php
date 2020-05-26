36e91eeb7eff5a58878fc0952c2aefede9d2e641
Ricky <ricky.gamer000@gmail.com>
2020-05-26 13:27:26 -0400

    Added support for minutes by converting all time to seconds

--- fastforward.php
+++ fastforward.php
@@ -8,7 +8,7 @@
  * The variables below are assigned values from the link/query the User clicked on. 
  */
  	$videoId = (string) $_GET['VideoID']; // type-casted to string, however, (int) should be used if only numbers are expected, for added security. Set by query.
- 	$startAt = (int) $_GET['StartAt']; // specific place-in-time to start youtube video, as set by query.
+ 	$startAt = (string) $_GET['StartAt']; // specific place-in-time to start youtube video, as set by query.
  	$version = (int) $_GET['Version']; // type-casted to int as a version should be a number. Set by query.
  /**	
  * Hard-code link to help page.
@@ -22,8 +22,43 @@
  * Hard-code Youtube video links by adding them into this arrayOfVideos.
  * The number pointing to the URL represents the VideoID to match. 
  */
-	$arrayOfVideos = array(1=>'https://youtu.be/c_fUcDsgSjA',
-						   2=>'https://youtu.be/MnR9AVs6Q_c');
+	$arrayOfVideos = array(1=>'https://youtu.be/6avJHaC3C2U',
+						   2=>'https://youtu.be/6avJHaC3C2U');
+
+/**
+ * Let's check if video start is in seconds or minutes.
+ * You can specify minutes using a colon, for example,
+ * Ten minutes and 30 seconds would look like 10:30
+ * 
+ * The URL query would look like StartAt=10:30
+ */
+	function isStartTimeInSecondsOrMinutes($startAt)
+	{
+		if (strstr($startAt,':') === false) {
+			return 'seconds';
+		} 
+		return 'minutes';
+	}
+
+	function getMinutes($startAt)
+	{
+		return strstr($startAt,':') === false ? 0 : strstr($startAt,':',true);
+	}
+
+	function getSeconds($startAt)
+	{
+		return strstr($startAt,':') === false ? 0 : substr(strstr($startAt,':'),1);
+	}
+
+	function convertStartTimeToSeconds($startAt)
+	{
+		if (isStartTimeInSecondsOrMinutes($startAt) === 'minutes') {
+			$minutes = getMinutes($startAt);
+			$seconds = getSeconds($startAt);
+			return ($minutes * 60) + $seconds;
+		}
+		return $startAt;
+	}
 /**
  * If VideoID is NOT empty
  */ 
@@ -39,12 +74,13 @@ if ($videoId != '') {
 	} else {
 		
 			// this will redirect the User to the defined video, by ID, found matching in arrayOfVideos.
-			header('Location: '.$arrayOfVideos[$videoId].'?t='.$startAt.'s');
+			header('Location: '.$arrayOfVideos[$videoId].'?t='.convertStartTimeToSeconds($startAt).'s');
 			exit();
 
 	}
 }
-
+print_r('to far');
+die();
 //WHEN ALL ELSE FAILS SEND USER TO ERROR PAGE.
 header("Location: $errorPage");
 exit();	
